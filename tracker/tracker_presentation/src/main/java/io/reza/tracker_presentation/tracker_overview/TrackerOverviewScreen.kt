a608884e6170f6fa84e73e23c10ca1a4c2542244
Reza Faraji <faraji.reza@outlook.com>
2022-07-09 21:20:01 +0430

    Implemented NutrientInfo.kt on TrackerOverviewScreen.kt

--- tracker/tracker_presentation/src/main/java/io/reza/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/io/reza/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -2,15 +2,18 @@ package io.reza.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.annotation.ExperimentalCoilApi
 import io.reza.core.util.UiEvent
 import io.reza.core_ui.LocalSpacing
-import io.reza.tracker_presentation.tracker_overview.components.DaySelector
-import io.reza.tracker_presentation.tracker_overview.components.NutrientsHeader
+import io.reza.tracker_presentation.tracker_overview.components.*
 
+@ExperimentalCoilApi
 @Composable
 fun TrackerOverviewScreen(
     onNavigate: (UiEvent.Navigate) -> Unit,
@@ -38,6 +41,42 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach { trackedFood ->
+                            TrackedFoodItem(
+                                trackedFood = trackedFood,
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent.OnDeleteTrackedFoodClicked(
+                                            trackedFood
+                                        )
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = io.reza.core.R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = { viewModel.onEvent(TrackerOverviewEvent.OnAddFoodClicked(meal)) },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
+                onToggleClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClicked(meal)) },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 
